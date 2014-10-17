4b1f0d903183e5b94de202610618990439c6d03b
boywert <boywert@Chaichalits-MacBook-Pro.local>
2014-10-17 15:57:32 +0100

    quick commit

--- ionz_main.c
+++ ionz_main.c
@@ -213,7 +213,7 @@ int main(int argc, char **argv) {
   if(use_prev_xfrac == 1) {
     buffer = malloc(sizeof(float)*Nnion*N1*N2*N3);
     if(mympi.ThisTask == 0)
-      read_xfrac(outputdir, z_prev, buffer, robar,nion, Nnion, N1, N2, N3);
+      read_xfrac(outputdir, z_prev, buffer, nion, Nnion, N1, N2, N3);
 #ifdef PARALLEL
     MPI_Barrier(MPI_COMM_WORLD);
     MPI_Bcast(buffer, Nnion*N1*N2*N3, MPI_FLOAT, 0, MPI_COMM_WORLD);    
@@ -335,9 +335,9 @@ int main(int argc, char **argv) {
 #endif // PARALLEL  
   if(mympi.ThisTask == 0) {
 #ifdef PARALLEL
-    write_xfrac(outputdir, z_out, buffer_final, nh, nion, Nnion, N1, N2, N3);
+    write_xfrac(outputdir, z_out, buffer_final, nh, robar,nion, Nnion, N1, N2, N3);
 #else
-    write_xfrac(outputdir, z_out, buffer, nh, nion, Nnion, N1, N2, N3);
+    write_xfrac(outputdir, z_out, buffer, nh, nion, robar, Nnion, N1, N2, N3);
 #endif
   }
   MPI_Finalize();
