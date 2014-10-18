99be647104f731250dfff825652640c1290270d4
boywert <boywert@Chaichalits-MacBook-Pro.local>
2014-10-18 23:52:38 +0100

    quick commit

--- ion.h
+++ ion.h
@@ -128,7 +128,7 @@ extern void reionization_with_xfrac(float Radii,fftw_real ***nh_p, fftw_real ***
 #ifdef __cplusplus
 extern "C" {
 #endif
-  void __arrayoperations_MOD_fortran_subgrid_reionization_with_xfrac(fftw_real *nh,fftw_real *ngamma, fftw_real *xfrac,fftw_real *nxion,int *nion,int *len) 
+  extern void __arrayoperations_MOD_fortran_subgrid_reionization_with_xfrac(fftw_real *nh,fftw_real *ngamma, fftw_real *xfrac,fftw_real *nxion,int *nion,int *len);
 #ifdef __cplusplus
 #endif
 #define ION_H_
