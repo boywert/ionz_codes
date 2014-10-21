50d682b72406a0b7949c56edcbd9ad47051cde27
boywert <boywert@Chaichalits-MacBook-Pro.local>
2014-10-21 13:08:21 +0100

    quick commit

--- plot_xfrac.py
+++ plot_xfrac.py
@@ -19,5 +19,5 @@ zlist = open("/mnt/lustre/scratch/cs390/47Mpc/snap_z3.txt").readlines()
 for z in zlist:
     z = z.strip()
     data = read_xfrac("/mnt/lustre/scratch/cs390/codes/ionz_codes/usexfrac/40000.00/xfrac3d_"+z+".bin")
-    suma = data.sum/float(306**3)
+    suma = data.sum/306./306/306.
     print z,suma
