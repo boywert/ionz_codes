12bed96d61fc950da1e65b66ddc56eaaecdddd0a
boywert <boywert@Chaichalits-MacBook-Pro.local>
2014-10-21 13:18:27 +0100

    quick commit

--- plot_xfrac.py
+++ plot_xfrac.py
@@ -3,12 +3,12 @@ import numpy
 def read_xfrac(file):
     #print "reading",file
     f = open(file,"rb")
-    dummy = numpy.fromfile(f,numpy.int32,1)[0]
+    #dummy = numpy.fromfile(f,numpy.int32,1)[0]
     ngrid = numpy.fromfile(f,numpy.int32,3)
-    dummy = numpy.fromfile(f,numpy.int32,1)[0]
-    dummy = numpy.fromfile(f,numpy.int32,1)[0]
+    #dummy = numpy.fromfile(f,numpy.int32,1)[0]
+    #dummy = numpy.fromfile(f,numpy.int32,1)[0]
     data = numpy.fromfile(f,numpy.float32,ngrid[0]**3)
-    dummy = numpy.fromfile(f,numpy.int32,1)[0]
+    #dummy = numpy.fromfile(f,numpy.int32,1)[0]
     #print ngrid[0]**3,len(data)
     #print numpy.sum(data)/float(ngrid[0]**3)
     f.close()
@@ -16,9 +16,10 @@ def read_xfrac(file):
 
 
 zlist = open("/mnt/lustre/scratch/cs390/47Mpc/snap_z3.txt").readlines()
-
+base = "/mnt/lustre/scratch/cs390/codes/ionz_codes/usexfrac/40000.00"
+#base = "/mnt/lustre/scratch/cs390/47Mpc/couple/model_001/xfrac/40000.00/40000.00"
 for z in zlist:
     z = z.strip()
-    data = read_xfrac("/mnt/lustre/scratch/cs390/47Mpc/couple/model_001/xfrac/40000.00/40000.00/xfrac3d_"+z+".bin")
+    data = read_xfrac(base+"/xfrac3d_"+z+".bin")
     suma = numpy.sum(data)/float(306**3)
     print z,suma
