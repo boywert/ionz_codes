3efc031ddac3143909e040a4265e223d71307976
boywert <boywert@Chaichalits-MacBook-Pro.local>
2014-10-21 13:10:25 +0100

    quick commit

--- plot_xfrac.py
+++ plot_xfrac.py
@@ -7,7 +7,7 @@ def read_xfrac(file):
     ngrid = numpy.fromfile(f,numpy.int32,3)
     #dummy = numpy.fromfile(f,numpy.int32,1)[0]
     #dummy = numpy.fromfile(f,numpy.int32,1)[0]
-    data = numpy.fromfile(f,numpy.float32)
+    data = numpy.fromfile(f,numpy.float32,ngrid[0]**3)
     #dummy = numpy.fromfile(f,numpy.int32,1)[0]
     #print ngrid[0]**3,len(data)
     f.close()
@@ -19,5 +19,5 @@ zlist = open("/mnt/lustre/scratch/cs390/47Mpc/snap_z3.txt").readlines()
 for z in zlist:
     z = z.strip()
     data = read_xfrac("/mnt/lustre/scratch/cs390/codes/ionz_codes/usexfrac/40000.00/xfrac3d_"+z+".bin")
-    suma = data.sum/306./306/306.
+    suma = numpy.sum(data)/306./306/306.
     print z,suma
