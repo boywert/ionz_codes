895724297be3b92ab741f1e143a05b49cf878f5a
boywert <boywert@Chaichalits-MacBook-Pro.local>
2014-10-21 13:03:59 +0100

    quick commit

--- plot_xfrac.py
+++ plot_xfrac.py
@@ -1,7 +1,7 @@
 import numpy
 
 def read_xfrac(file):
-    print "reading",file
+    #print "reading",file
     f = open(file,"rb")
     #dummy = numpy.fromfile(f,numpy.int32,1)[0]
     ngrid = numpy.fromfile(f,numpy.int32,3)
@@ -9,7 +9,8 @@ def read_xfrac(file):
     #dummy = numpy.fromfile(f,numpy.int32,1)[0]
     data = numpy.fromfile(f,numpy.float32)
     #dummy = numpy.fromfile(f,numpy.int32,1)[0]
-    print ngrid[0]**3,len(data)
+    #print ngrid[0]**3,len(data)
+    f.close()
     return data
 
 
