a8536e918d99266175fe58c774580e94e5f88b7c
boywert <boywert@Chaichalits-MacBook-Pro.local>
2014-10-21 13:13:42 +0100

    quick commit

--- plot_xfrac.py
+++ plot_xfrac.py
@@ -18,6 +18,6 @@ zlist = open("/mnt/lustre/scratch/cs390/47Mpc/snap_z3.txt").readlines()
 
 for z in zlist:
     z = z.strip()
-    data = read_xfrac("/mnt/lustre/scratch/cs390/codes/ionz_codes/usexfrac/40000.00/xfrac3d_"+z+".bin")
+    data = read_xfrac("/mnt/lustre/scratch/cs390/47Mpc/couple/model_001/xfrac/40000.00/40000.00/xfrac3d_"+z+".bin")
     suma = numpy.sum(data)
     print z,suma
