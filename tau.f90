c1948d7680758e62f80580d12e3cf97d293d1288
Chaichalit Srisawat <boywert@gmail.com>
2015-11-03 02:30:41 +0000

    quick commit

--- tau.f90
+++ tau.f90
@@ -11,7 +11,7 @@
       real*8,dimension(100) :: frac_m, frac_v, z, tau_es, tau_es_full
       real*8 :: tau0, coeff, zred
 
-      open(1,file='runall_oka_infall.pbs.log')
+      open(1,file='runall_nore_infall.pbs.log')
       open(2,file='data.tau')
 
       do i=1,100
